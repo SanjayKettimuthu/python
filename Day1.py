8768de1481c8af6f775e19fde153d5dba26d80b4
Sanjay Kettimuthu <sanjaykettimuthu@protonmail.com>
2024-10-14 19:50:23 +0530

    day1 completed

---
+++ Day1.py
@@ -0,0 +1,19 @@
+#python variables
+userName = input("What is your name?")
+#print("Hello " + userName + "!")
+#print("The length of the user name is" + " " + str(len(userName)))
+userNameLen = len(userName)
+print("The userName is" + " " + userName)
+print("The userNamelen is " + str(userNameLen))
+glass1 = "milk"
+glass2 = "juice"
+medium = glass1
+glass1 = glass2
+glass2 = medium
+print("glass1" + " is " + glass1 + " " + "glass2" + " is" + " " + glass2)
+#variable naming
+#Band Name generator
+print("Hello there welcome to the band name generator")
+userCity = input("Enter the name of the city that you grew up in:")
+userPet = input("Enter the name of the pet:")
+print("The band name is" + " " + userCity + " " + userPet)
